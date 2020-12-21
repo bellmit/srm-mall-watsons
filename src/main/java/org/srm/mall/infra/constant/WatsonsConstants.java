9aab94fc405691bd1fb842495f73635bec7b8302
吉福21419 <fu.ji@hand-china.com>
2020-12-21 14:35:11 +0800

    [ADD]添加常量

---
+++ src/main/java/org/srm/mall/infra/constant/WatsonsConstants.java
@@ -0,0 +1,12 @@
+package org.srm.mall.infra.constant;
+
+/**
+ * @author fu.ji@hand-china.com 2020-12-21 14:34:22
+ */
+public interface WatsonsConstants {
+    /**
+     * 租户编码
+     */
+    String TENANT_NUMBER = "SRM-WATSONS";
+
+}
