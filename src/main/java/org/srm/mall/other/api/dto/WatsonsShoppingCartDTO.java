2fc578411c691a338bcdc6c2c867ecaba526c6a2
魏岳文19635 <yuewen.wei@hand-china.com>
2020-12-25 11:36:02 +0800

    [FIX] 费用分配

--- src/main/java/org/srm/mall/other/api/dto/WatsonsShoppingCartDTO.java
+++ src/main/java/org/srm/mall/other/api/dto/WatsonsShoppingCartDTO.java
@@ -2,20 +2,12 @@ package org.srm.mall.other.api.dto;
 
 import io.choerodon.core.exception.CommonException;
 import io.swagger.annotations.ApiModelProperty;
-import org.hzero.core.base.BaseConstants;
-import org.hzero.core.util.ResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.srm.mall.common.feign.SmdmRemoteService;
-import org.srm.mall.context.entity.ItemCategory;
-import org.srm.mall.other.api.dto.ShoppingCartDTO;
 import org.srm.mall.other.app.service.impl.ShoppingCartServiceImpl;
 import org.srm.mall.other.domain.entity.AllocationInfo;
-import org.srm.mall.other.domain.entity.WatsonsShoppingCart;
-import org.srm.mall.platform.domain.entity.PurReqMergeRule;
-import org.srm.mall.product.api.dto.ItemCategoryDTO;
 
 
 import java.util.List;
