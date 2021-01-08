a536ae93fe09bdd1d19089a3d4754b8e4c4f8434
张楗豪29857 <OfferGiles@hotmail.com>
2021-01-08 15:12:04 +0800

    增加查询费用项目值集接口

--- src/main/java/org/srm/mall/common/feign/fallback/ProjectCostRemoteServiceFallbackImpl.java
+++ src/main/java/org/srm/mall/common/feign/fallback/ProjectCostRemoteServiceFallbackImpl.java
@@ -4,6 +4,7 @@ import io.choerodon.mybatis.pagehelper.domain.PageRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.srm.mall.common.feign.ProjectCostRemoteService;
 import org.srm.mall.other.domain.entity.ProjectCost;
 
@@ -12,6 +13,8 @@ import org.srm.mall.other.domain.entity.ProjectCost;
  *
  * @author jianhao.zhang01@hand-china.com 2021/01/07 18:30
  */
+
+@Component
 public class ProjectCostRemoteServiceFallbackImpl implements ProjectCostRemoteService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectCostRemoteServiceFallbackImpl.class);
