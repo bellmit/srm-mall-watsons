bdb948b03cee79a989829fe18bd645951b29cfb3
张楗豪29857 <offergiles@hotmail.com>
2021-03-19 17:56:10 +0800

    [ADD]添加wfl校验

--- src/main/java/org/srm/mall/common/feign/fallback/WatsonsWflCheckRemoteServiceFallbackImpl.java
+++ src/main/java/org/srm/mall/common/feign/fallback/WatsonsWflCheckRemoteServiceFallbackImpl.java
@@ -3,6 +3,7 @@ package org.srm.mall.common.feign.fallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.srm.mall.common.feign.WatsonsWflCheckRemoteService;
 import org.srm.mall.common.feign.dto.wflCheck.WatsonsWflCheckDTO;
 import org.srm.mall.common.feign.dto.wflCheck.WatsonsWflCheckResultVO;
@@ -14,6 +15,7 @@ import java.util.List;
  *
  * @author ericzhang 2021/03/19 3:50 下午
  */
+@Component
 public class WatsonsWflCheckRemoteServiceFallbackImpl implements WatsonsWflCheckRemoteService {
 
     private Logger logger = LoggerFactory.getLogger(WatsonsWflCheckRemoteServiceFallbackImpl.class);
